62d4b2ffba502ff7f73a362a8e28532aa042e1e1
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-19 19:51:14 +0200

    feat: some updates

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,16 @@ use crate::args::Command;
 pub mod commands;
 pub mod args;
 pub mod colors;
-mod statics;
+pub mod statics;
+pub mod log;
 
 fn main() {
     let args = args::Args::collect();
     let options = args::Args::collect().collect_options();
 
     match args.command {
-        Command::Install => {
-
-        },
+        Command::Install => commands::install::install(options.file.unwrap()),
+        Command::Extract => commands::extract::extract(options.file.unwrap(), options.destination.unwrap()),
         Command::None => commands::help::help(),
     }
-    println!("Hello, world!");
 }
