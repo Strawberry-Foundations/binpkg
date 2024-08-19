1525ae5ec889cfcb2c61cda5bc8b9e5e2256b9a0
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-19 19:49:36 +0200

    feat: add install command

---
+++ src/commands/install.rs
@@ -0,0 +1,22 @@
+use libspkg::binpkg::BinPkg;
+use libspkg::binpkg::err::BinPkgError;
+use crate::commands::extract::extract;
+use crate::{log_fail, log_info};
+
+pub fn install(file: String) {
+    let package = match BinPkg::read(&file) {
+        Ok(package) => {
+            package
+        },
+        Err(err) => {
+            let _: BinPkgError = *err.downcast_ref().unwrap();
+            log_fail!(format!("Error while reading package: {err}"));
+            std::process::exit(1);
+        }
+    };
+    
+    extract(file.clone(), "/tmp/_binpkg.workdir".to_string());
+    
+    log_info!(format!("Installing {} version {}", package.metadata.id, package.metadata.version));
+    subprocess::Exec::shell("cp -r /tmp/_binpkg.workdir/* /").popen().unwrap();
+}
