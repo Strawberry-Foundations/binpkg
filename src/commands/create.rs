5429a58055778d368823c220733b32ce7521b8e1
Juliandev02 <julian@strawberryfoundations.xyz>
2024-09-02 18:47:42 +0200

    feat: add 'create' command

---
+++ src/commands/create.rs
@@ -0,0 +1,9 @@
+use libspkg::binpkg::BinPkg;
+use libspkg::binpkg::err::BinPkgError;
+
+use crate::colors::{CYAN, RESET};
+use crate::{log_fail, log_info};
+
+pub fn create(file: String, destination: String) {
+
+}
